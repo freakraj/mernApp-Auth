899be3547a03167378ebd406ca898db46cdb5494
Raj Gautam <raj.gautam@dtdc.com>
2024-06-14 03:03:26 +0530

    connect to mongoDb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("MongoDB connected...");
+//   })
+//   .catch((err) => {console.log(err)});
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('server listening on port 3000')
-})
+
+app.listen(3000, async () => {
+    try {
+      await mongoose.connect.uri
+      console.log("connect mongdb")
+    } catch (error) {
+      console.log(error);
+    }
+  console.log("Server listening on port 3000");
+});
