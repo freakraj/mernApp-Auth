beca01cb20408d53c84e95cbfd4bdecd257fac37
Raj Gautam <raj.gautam@dtdc.com>
2024-06-21 17:03:58 +0530

    complete image upload functionality.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,45 +1,109 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
-  // console.log("hello gautam",currentUser);
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [formData, setFormData] = useState({});
+  const [imageError, setImageError] = useState(false);
+  // console.log(imagePercent);
+  // console.log(formData);
+
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
     <div className="max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
         <img
           src={currentUser.profilePicture}
           alt="profile"
           className="h-24 w-24 self-center rounded-full cursor-pointer object-cover mt-2"
+          onClick={() => fileRef.current.click()}
         />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${imagePercent} %`}</span>
+          ) : imagePercent === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           defaultValue={currentUser.username}
           type="text"
-          className="bg-slate-100 rounded-lg p-3 "
+          className="bg-slate-100 rounded-lg p-3"
           id="username"
           placeholder="Username"
         />
         <input
           defaultValue={currentUser.email}
           type="email"
-          className="bg-slate-100 rounded-lg p-3 "
+          className="bg-slate-100 rounded-lg p-3"
           id="email"
           placeholder="Email"
         />
         <input
           type="password"
-          className="bg-slate-100 rounded-lg p-3 "
+          className="bg-slate-100 rounded-lg p-3"
           id="password"
           placeholder="Password"
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          update
+          Update
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer ">Delete Account</span>
-        <span className="text-red-700 cursor-pointer ">Sign out</span>
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
     </div>
   );
